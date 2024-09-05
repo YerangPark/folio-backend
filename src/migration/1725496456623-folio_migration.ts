79096d139cb4e1f562006a18b7202026ebc92b14
YerangPark <smsms_@naver.com>
2024-09-05 10:45:35 +0900

    test: workflow 파일에 SSH 터널링 관련 테스트 구문 추가

---
+++ src/migration/1725496456623-folio_migration.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class FolioMigration1725496456623 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
