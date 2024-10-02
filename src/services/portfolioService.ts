18df79a7a651008bb7fa6d39c230dcb58af08818
YerangPark <smsms_@naver.com>
2024-10-02 10:00:00 +0900

    feat: 서비스단 포트폴리오 수정 로직 수정

--- src/services/portfolioService.ts
+++ src/services/portfolioService.ts
@@ -10,6 +10,8 @@ import { SkillEntity } from '../entities/skillEntity.js';
 import { ProjectEntity } from '../entities/projectEntity.js';
 import { PortfolioSkillEntity } from '../entities/portfolioSkillEntity.js';
 import { ProjectSkillEntity } from '../entities/projectSkillEntity.js';
+import path from 'path';
+import fs from 'fs';
 
 interface ProjectSkill {
   id: number;
@@ -24,6 +26,7 @@ interface Project {
   github_link?: string;
   site_link?: string;
   description: string;
+  readme_file?: string;
   skills?: ProjectSkill[];
 }
 
@@ -39,9 +42,21 @@ interface Portfolio {
   description: string;
   github_link?: string;
   blog_link?: string;
+  image?: string;
   skills?: PortfolioSkill[];
   projects?: Project[];
 }
+
+const deleteFile = (filePath: string) => {
+  const fullPath = path.join(process.env.UPLOAD_PATH || '', filePath);
+  fs.unlink(fullPath, (err) => {
+    if (err) {
+      console.error(`Failed to delete file: ${fullPath}`, err);
+    } else {
+      console.log(`Successfully deleted file: ${fullPath}`);
+    }
+  });
+};
 
 class PortfolioService {
   static portfolioRepository: Repository<PortfolioEntity> = AppDataSource.getRepository(PortfolioEntity);
@@ -53,25 +68,25 @@ class PortfolioService {
   // 포트폴리오 생성
   static async createPortfolio(userId: number, portfolioData: Portfolio): Promise<PortfolioEntity> {
     const queryRunner: QueryRunner = AppDataSource.createQueryRunner();
-
     await queryRunner.connect();
     await queryRunner.startTransaction();
 
     try {
-      // 유저 ID가 반드시 있어야 한다는 검증 추가
       if (!userId) {
         throw new CustomError(HTTP_STATUS.BAD_REQUEST, 'USER_ID_REQUIRED', 'User ID is required to create a portfolio.');
       }
 
-      const existingPortfolio = await AppDataSource.getRepository(PortfolioEntity).findOne({where: { file_name: portfolioData.file_name, user: { id: userId } }});
+      const existingPortfolio = await AppDataSource.getRepository(PortfolioEntity).findOne({
+        where: { file_name: portfolioData.file_name, user: { id: userId } },
+      });
+
       if (existingPortfolio) {
         throw new CustomError(HTTP_STATUS.BAD_REQUEST, 'FILENAME_TAKEN', ERROR_MESSAGES.FILENAME_TAKEN);
       }
 
-      // const user = await UserModel.findByUserid(userId);
       const user = await queryRunner.manager.findOne(UserEntity, { where: { id: userId } });
       if (!user) {
-        throw new Error('@@@@@@@@@@@@@@@@@@ User not found');
+        throw new Error('User not found');
       }
 
       const portfolio = new PortfolioEntity();
@@ -81,36 +96,26 @@ class PortfolioService {
       portfolio.github_link = portfolioData.github_link as string;
       portfolio.blog_link = portfolioData.blog_link as string;
       portfolio.user = Promise.resolve(user);
+      portfolio.image = portfolioData.image as string;
       const newPortfolio = await this.portfolioRepository.save(portfolio);
 
-      // const newPortfolio = await PortfolioModel.createPortfolio({
-      //   user_id: user.id, // 명시적으로 userId 전달
-      //   user: Promise.resolve(user), // lazy loading 관계 설정
-      //   file_name: portfolioData.file_name,
-      //   title: portfolioData.title,
-      //   description: portfolioData.description,
-      //   github_link: portfolioData.github_link,
-      //   blog_link: portfolioData.blog_link,
-      // });
-
+      // 스킬 연결 처리
       if (portfolioData.skills) {
-        for (const skill of portfolioData.skills) {
-          const skillEntity = await this.skillRepository.findOne({ where: { id: skill.id } });
+        const skillsArray = typeof portfolioData.skills === 'string' ? JSON.parse(portfolioData.skills) : portfolioData.skills;
+        for (const skill of skillsArray) {
+              const skillEntity = await queryRunner.manager.findOne(SkillEntity, { where: { id: skill } });
           if (!skillEntity) {
             throw new CustomError(HTTP_STATUS.BAD_REQUEST, 'SKILL_NOT_FOUND', ERROR_MESSAGES.SKILL_NOT_FOUND);
           }
-          // await PortfolioModel.addSkillToPortfolio(newPortfolio, skillEntity);
+
           const portfolioSkill = new PortfolioSkillEntity();
-
-          // Lazy-loaded 관계의 Promise를 await으로 풀어서 실제 객체를 할당
-          portfolioSkill.portfolio = Promise.resolve(newPortfolio);  // lazy로 인해 Promise로 감싸줌
-          portfolioSkill.skill = Promise.resolve(skillEntity);  // lazy로 인해 Promise로 감싸줌
-
-          // 저장
-          await this.portfolioSkillRepository.save(portfolioSkill);
+          portfolioSkill.portfolio = Promise.resolve(newPortfolio);
+          portfolioSkill.skill = Promise.resolve(skillEntity);
+          await queryRunner.manager.save(portfolioSkill);
         }
       }
 
+      // 프로젝트 처리
       if (portfolioData.projects) {
         for (const project of portfolioData.projects) {
           const tempProject = new ProjectEntity();
@@ -120,35 +125,24 @@ class PortfolioService {
           tempProject.end_date = project.end_date;
           tempProject.github_link = project.github_link as string;
           tempProject.site_link = project.site_link as string;
+          tempProject.readme_file = project.readme_file as string;
           tempProject.description = project.description;
           tempProject.portfolio = Promise.resolve(newPortfolio);
-          const newProject = await this.projectRepository.save(tempProject);
-          // const newProject = await PortfolioModel.createProject({
-          //   name: project.name,
-          //   image: project.image,
-          //   start_date: project.start_date,
-          //   end_date: project.end_date,
-          //   github_link: project.github_link,
-          //   site_link: project.site_link,
-          //   description: project.description,
-          //   portfolio: Promise.resolve(newPortfolio),
-          // });
+
+          const newProject = await queryRunner.manager.save(tempProject);
 
           if (project.skills) {
-            for (const skill of project.skills) {
-              const skillEntity = await this.skillRepository.findOne({ where: { id: skill.id } });
+            const skillsArray = typeof project.skills === 'string' ? JSON.parse(project.skills) : project.skills;
+            for (const skill of skillsArray) {
+              const skillEntity = await queryRunner.manager.findOne(SkillEntity, { where: { id: skill } });
               if (!skillEntity) {
                 throw new CustomError(HTTP_STATUS.BAD_REQUEST, 'SKILL_NOT_FOUND', ERROR_MESSAGES.SKILL_NOT_FOUND);
               }
-              // await PortfolioModel.addSkillToProject(newProject, skillEntity);
+
               const projectSkill = new ProjectSkillEntity();
-
-              // Lazy-loaded 관계의 Promise를 await으로 풀어서 실제 객체를 할당
-              projectSkill.project = Promise.resolve(newProject);  // lazy로 인해 Promise로 감싸줌
-              projectSkill.skill = Promise.resolve(skillEntity);  // lazy로 인해 Promise로 감싸줌
-
-              // 저장
-              await this.projectSkillRepository.save(projectSkill);
+              projectSkill.project = Promise.resolve(newProject);
+              projectSkill.skill = Promise.resolve(skillEntity);
+              await queryRunner.manager.save(projectSkill);
             }
           }
         }
@@ -168,80 +162,174 @@ class PortfolioService {
   static async getPortfolioById(id: number): Promise<PortfolioEntity | null> {
     const portfolio = await this.portfolioRepository.findOne({
       where: { id },
-      relations: ['projects', 'portfolioSkills'],
+      relations: ['projects', 'portfolioSkills', 'projects.projectSkills', 'projects.projectSkills.skill'],
     });
+
     if (!portfolio) {
       throw new CustomError(HTTP_STATUS.NOT_FOUND, 'PORTFOLIO_NOT_FOUND', 'Portfolio not found.');
     }
+
+    // 이미지 경로에서 파일명만 추출
+    if (portfolio.image) {
+      portfolio.image = portfolio.image.split('/').pop() || portfolio.image;
+    }
+
+    const projects = await Promise.all(
+      (await portfolio.projects).map(async project => {
+        if (project.image) {
+          project.image = project.image.split('/').pop() || project.image;
+        }
+        if (project.readme_file) {
+          project.readme_file = project.readme_file.split('/').pop() || project.readme_file;
+        }
+        return project;
+      })
+    );
+    portfolio.projects = Promise.resolve(projects);
+
     return portfolio;
   }
 
   // 포트폴리오 업데이트
-  static async updatePortfolio(id: number, updatedData: Portfolio): Promise<void> {
-    const existingPortfolio = await this.portfolioRepository.findOne({
-      where: { id },
-      relations: ['projects', 'portfolioSkills'],
-    });
-    if (!existingPortfolio) {
-      throw new CustomError(HTTP_STATUS.NOT_FOUND, 'PORTFOLIO_NOT_FOUND', 'Portfolio not found.');
-    }
+  static async updatePortfolio(id: number, updatedData: Portfolio): Promise<PortfolioEntity> {
+    const queryRunner: QueryRunner = AppDataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
 
-    existingPortfolio.title = updatedData.title;
-    existingPortfolio.description = updatedData.description;
-    existingPortfolio.github_link = updatedData.github_link as string;
-    existingPortfolio.blog_link = updatedData.blog_link as string;
-    await this.portfolioRepository.update(id, existingPortfolio);
-
-    // 추가된 스킬들 업데이트
-    if (updatedData.skills) {
-      for (const skill of updatedData.skills) {
-        const skillEntity = await this.skillRepository.findOne({ where: { id: skill.id } });
-        if (!skillEntity) {
-          throw new CustomError(HTTP_STATUS.BAD_REQUEST, 'SKILL_NOT_FOUND', ERROR_MESSAGES.SKILL_NOT_FOUND);
+    try {
+        const existingPortfolio = await queryRunner.manager.findOne(PortfolioEntity, {
+            where: { id },
+            relations: ['projects', 'portfolioSkills'],
+        });
+        if (!existingPortfolio) {
+            throw new CustomError(HTTP_STATUS.NOT_FOUND, 'PORTFOLIO_NOT_FOUND', 'Portfolio not found.');
         }
-        const portfolioSkill = new PortfolioSkillEntity();
 
-        // Lazy-loaded 관계의 Promise를 await으로 풀어서 실제 객체를 할당
-        portfolioSkill.portfolio = Promise.resolve(existingPortfolio);  // lazy로 인해 Promise로 감싸줌
-        portfolioSkill.skill = Promise.resolve(skillEntity);  // lazy로 인해 Promise로 감싸줌
+        // 포트폴리오 기본 정보 업데이트
+        existingPortfolio.file_name = updatedData.file_name;
+        existingPortfolio.title = updatedData.title;
+        existingPortfolio.description = updatedData.description;
+        existingPortfolio.github_link = updatedData.github_link as string;
+        existingPortfolio.blog_link = updatedData.blog_link as string;
+        if (updatedData.image) {
+            existingPortfolio.image = updatedData.image;  // 이미지 업데이트 처리
+        }
+        await queryRunner.manager.save(existingPortfolio);
 
-        // 저장
-        await this.portfolioSkillRepository.save(portfolioSkill);
+        // 스킬 업데이트
+        if (updatedData.skills) {
+          const skillsArray = typeof updatedData.skills === 'string' ? JSON.parse(updatedData.skills) : updatedData.skills;
+
+          // 기존 스킬을 모두 삭제하는 대신, 중복되지 않는 새로운 스킬만 추가
+          const existingSkills = await queryRunner.manager.find(PortfolioSkillEntity, {
+              where: { portfolio: { id } },
+              relations: ['skill'],
+          });
+
+          for (const skill of skillsArray) {
+              const skillEntity = await queryRunner.manager.findOne(SkillEntity, { where: { id: skill } });
+              if (!skillEntity) {
+                  throw new CustomError(HTTP_STATUS.BAD_REQUEST, 'SKILL_NOT_FOUND', ERROR_MESSAGES.SKILL_NOT_FOUND);
+              }
+
+              const isSkillAlreadyAdded = existingSkills.some(existingSkill => existingSkill.skill_id === skill);
+
+              // 중복된 스킬이 아니라면 새롭게 추가
+              if (!isSkillAlreadyAdded) {
+                  const portfolioSkill = new PortfolioSkillEntity();
+                  portfolioSkill.portfolio = Promise.resolve(existingPortfolio);
+                  portfolioSkill.skill = Promise.resolve(skillEntity);
+                  await queryRunner.manager.save(portfolioSkill);
+              }
+          }
       }
-    }
 
-    // 추가된 프로젝트들 업데이트
-    if (updatedData.projects) {
-      for (const project of updatedData.projects) {
-        const tempProject = new ProjectEntity();
-        tempProject.name = project.name;
-        tempProject.image = project.image as string;
-        tempProject.start_date = project.start_date;
-        tempProject.end_date = project.end_date;
-        tempProject.github_link = project.github_link as string;
-        tempProject.site_link = project.site_link as string;
-        tempProject.description = project.description;
-        tempProject.portfolio = Promise.resolve(existingPortfolio);
-        const newProject = await this.projectRepository.save(tempProject);
+        // 프로젝트 업데이트
+        if (updatedData.projects) {
+          const existingProjects = await queryRunner.manager.find(ProjectEntity, { where: { portfolio: { id } }, relations: ['projectSkills'] });
 
-        if (project.skills) {
-          for (const skill of project.skills) {
-            const skillEntity = await this.skillRepository.findOne({ where: { id: skill.id } });
-            if (!skillEntity) {
-              throw new CustomError(HTTP_STATUS.BAD_REQUEST, 'SKILL_NOT_FOUND', ERROR_MESSAGES.SKILL_NOT_FOUND);
-            }
-            // await PortfolioModel.addSkillToProject(newProject, skillEntity);
-            const projectSkill = new ProjectSkillEntity();
+          for (const project of updatedData.projects) {
+              const existingProject = existingProjects.find(p => p.id === project.id);
 
-            // Lazy-loaded 관계의 Promise를 await으로 풀어서 실제 객체를 할당
-            projectSkill.project = Promise.resolve(newProject);  // lazy로 인해 Promise로 감싸줌
-            projectSkill.skill = Promise.resolve(skillEntity);  // lazy로 인해 Promise로 감싸줌
+              if (existingProject) {
+                  // 기존 프로젝트가 있으면 업데이트
+                  existingProject.name = project.name;
+                  existingProject.image = project.image as string;
+                  existingProject.start_date = project.start_date;
+                  existingProject.end_date = project.end_date;
+                  existingProject.github_link = project.github_link as string;
+                  existingProject.site_link = project.site_link as string;
+                  existingProject.description = project.description;
 
-            // 저장
-            await this.projectSkillRepository.save(projectSkill);
+                  // 프로젝트 저장 (업데이트)
+                  await queryRunner.manager.save(existingProject);
+
+                  // 프로젝트에 연결된 스킬 업데이트
+                  if (project.skills) {
+                      const projectSkillsArray = typeof project.skills === 'string' ? JSON.parse(project.skills) : project.skills;
+                      // 기존 프로젝트 스킬을 삭제 후 다시 추가
+                      await queryRunner.manager.delete(ProjectSkillEntity, { project: { id: existingProject.id } });
+
+                      for (const skill of projectSkillsArray) {
+                          const skillEntity = await queryRunner.manager.findOne(SkillEntity, { where: { id: skill } });
+                          if (!skillEntity) {
+                              throw new CustomError(HTTP_STATUS.BAD_REQUEST, 'SKILL_NOT_FOUND', ERROR_MESSAGES.SKILL_NOT_FOUND);
+                          }
+
+                          const projectSkill = new ProjectSkillEntity();
+                          projectSkill.project = Promise.resolve(existingProject);
+                          projectSkill.skill = Promise.resolve(skillEntity);
+                          await queryRunner.manager.save(projectSkill);
+                      }
+                  }
+              } else {
+                  // 새로운 프로젝트 추가
+                  const newProject = new ProjectEntity();
+                  newProject.name = project.name;
+                  newProject.image = project.image as string;
+                  newProject.start_date = project.start_date;
+                  newProject.end_date = project.end_date;
+                  newProject.github_link = project.github_link as string;
+                  newProject.site_link = project.site_link as string;
+                  newProject.description = project.description;
+                  newProject.portfolio = Promise.resolve(existingPortfolio);
+
+                  await queryRunner.manager.save(newProject);
+
+                  // 새로운 프로젝트 스킬 추가
+                  if (project.skills) {
+                      const projectSkillsArray = typeof project.skills === 'string' ? JSON.parse(project.skills) : project.skills;
+                      for (const skill of projectSkillsArray) {
+                          const skillEntity = await queryRunner.manager.findOne(SkillEntity, { where: { id: skill } });
+                          if (!skillEntity) {
+                              throw new CustomError(HTTP_STATUS.BAD_REQUEST, 'SKILL_NOT_FOUND', ERROR_MESSAGES.SKILL_NOT_FOUND);
+                          }
+
+                          const projectSkill = new ProjectSkillEntity();
+                          projectSkill.project = Promise.resolve(newProject);
+                          projectSkill.skill = Promise.resolve(skillEntity);
+                          await queryRunner.manager.save(projectSkill);
+                      }
+                  }
+              }
+          }
+
+          // 기존 프로젝트 중 삭제된 프로젝트 처리
+          for (const existingProject of existingProjects) {
+              const projectExists = updatedData.projects.some(p => p.id === existingProject.id);
+              if (!projectExists) {
+                  await queryRunner.manager.delete(ProjectEntity, { id: existingProject.id });
+              }
           }
         }
-      }
+
+        await queryRunner.commitTransaction();
+        return existingPortfolio;
+    } catch (error) {
+        await queryRunner.rollbackTransaction();
+        throw error;
+    } finally {
+        await queryRunner.release();
     }
   }
 
@@ -261,10 +349,16 @@ class PortfolioService {
 
   // 특정 유저의 모든 포트폴리오 조회
   static async getPortfoliosByUserId(userId: number): Promise<PortfolioEntity[]> {
-    return await this.portfolioRepository.find({
+    const portfolios = await this.portfolioRepository.find({
       where: { user: { id: userId } },
       relations: ['projects', 'portfolioSkills'],
     });
+    portfolios.forEach((portfolio: PortfolioEntity)=> {
+      if (portfolio.image) {
+        portfolio.image = portfolio.image.split('/').pop() || portfolio.image;
+      }
+    })
+    return portfolios;
   }
 }
 
